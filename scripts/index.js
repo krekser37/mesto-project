a608cc231b610769b8dc0c2adf5ea4df05e42348
Ekaterina Kryuchkova <krekser37@yandex.ru>
2022-02-06 18:01:45 +0300

    feature/validation1

--- scripts/index.js
+++ scripts/index.js
@@ -155,3 +155,47 @@ formAddElement.addEventListener('submit', submitFormPhoto);
 initialCards.forEach(function(element) {
   renderCard(element.name, element.link, wrapElement) 
 }) 
+
+//Валидация
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('form__item-error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('form__input-error');
+}
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('form__item-error');
+  errorElement.classList.remove('form__input-error');
+  errorElement.textContent = '';
+}
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const setEventListener = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.form__item'));
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+      
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+    setEventListener(formElement);
+  });
+};
