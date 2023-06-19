96c5aeab3d2f0761b3ed0aacc989298b6cf2e57b
Ekaterina Kryuchkova <krekser37@yandex.ru>
2023-06-19 12:18:40 +0300

    fix/update

--- src/components/index.js
+++ src/components/index.js
@@ -129,7 +129,9 @@ api
 
 api.getAppInfo()
   .then(([cards, user]) => {
-    userInfo.setUserInfo(user.name, user.about, user.avatar);// принимает новые данные пользователя, отправляет их на сервер и добавляет их на страницу.
+    userInfo.setUserInfo(user.name, user.about, user.avatar);
+    // принимает новые данные пользователя, 
+    //отправляет их на сервер и добавляет их на страницу.
     userInfo.getUserInfo(user);// возвращает объект с данными пользователя
     currentUserId = user._id;
     section = defineSection(cards);
@@ -177,3 +179,5 @@ enableValidation(validationSettings);
 
 // или можно использовать строку (ведь Вы знаете, какой атрибут `name` у каждой формы)
 /* formValidators['profile-form'].resetValidation() */
+
+
